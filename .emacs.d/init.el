1510127587655d620e1db4b63a3ba85247b476fc
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-23 01:23:22 -0400

    Began the long journey of customizing my own EMACS

---
+++ .emacs.d/init.el
@@ -0,0 +1,40 @@
+;; package configs
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;; Cross over to the VIM DARK SIDE
+(use-package evil
+             :ensure t
+             :config
+             (evil-mode 1))
+
+(use-package base16-theme
+             :ensure t)
+
+;; Load all .el files under .emacs.d/config
+
+(load "~/.emacs.d/load-directory.el")
+(load-directory "~/.emacs.d/config")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
