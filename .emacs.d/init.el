7d82f70100f16df1a014f968a745597385e93b21
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-23 03:21:58 -0400

    It's getting quite cozy here over in Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,8 +8,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 ;; Bootstrap `use-package`
@@ -19,19 +19,19 @@
 (require 'use-package)
 
 ;; Use quelpa to enable use of some git repos
-;(use-package quelpa
-             ;:ensure t)
-;(quelpa
- ;'(quelpa-use-package
-   ;:fetcher git
-   ;:url "https://framagit.org/steckerhalter/quelpa-use-package.git"))
-;(require 'quelpa-use-package)
+					;(use-package quelpa
+					;:ensure t)
+					;(quelpa
+					;'(quelpa-use-package
+					;:fetcher git
+					;:url "https://framagit.org/steckerhalter/quelpa-use-package.git"))
+					;(require 'quelpa-use-package)
 
 ;; Cross over to the VIM DARK SIDE
 (use-package evil
-             :ensure t
-             :config
-             (evil-mode 1))
+  :ensure t
+  :config
+  (evil-mode 1))
 ;; Adds `fd` as an evil escape key
 (use-package evil-escape
   :ensure t
@@ -40,7 +40,7 @@
 
 ;; Theming framework
 (use-package base16-theme
-             :ensure t)
+  :ensure t)
 
 ;; Shows available keypresses ala spacemacs
 (use-package which-key
@@ -76,10 +76,39 @@
 	   :states '(normal visual insert emacs)
 	   :prefix "SPC"
 	   :non-normal-prefix "M-SPC"
+	   ;; Main arsenal
 	   "SPC" '(counsel-M-x :which-key "M-x")
 	   "/"   '(counsel-rg :which-key "ripgrep")
+	   ;; Files
+	   "ff"  '(counsel-find-file :which-key "find files")
+	   ;; Config
+	   "fei" (lambda ()
+		    (interactive)
+		    (split-window-right)
+		    (find-file user-init-file)
+		    :which-key "edit init.el")
+	   "fer" (lambda ()
+		    (interactive)
+		    (load-file user-init-file)
+		    :which-key "reload init.el")
+	   ;; Window creation and movement
+	   "wl"  '(windmove-right :which-key "move right")
+	   "wh"  '(windmove-left :which-key "move left")
+	   "wk"  '(windmove-up :which-key "move up")
+	   "wj"  '(windmove-down :which-key "move down")
+	   "wv"  '(split-window-right :which-key "split right")
+	   "ws"  '(split-window-below :which-key "split below")
+	   "wd"  '(delete-window :which-key "delete window")
+	   ;;Apps
+	   "att" '(ansi-term :which-key "open terminal")
+	   ;; Help
+	   "hdv" 'counsel-describe-variable
+	   "hdf" 'counsel-describe-function
+	   "hdk" 'describe-key
+	   ;; Comments
+	   "cl"  'comment-line
 	   ))
-	   
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
