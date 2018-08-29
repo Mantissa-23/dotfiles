1dcd18e3b83fb351b7220b889b39d1f8ae2d5fed
Mantissa-23 <dylanculfogienis@gmail.com>
2018-08-29 15:16:08 -0400

    Added custom.el to VCS

---
+++ .emacs.d/custom.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#292d3e" "#f07178" "#c3e88d" "#ffcb6b" "#82aaff" "#c792ea" "#82aaff" "#959dcb"])
+ '(company-quickhelp-mode t)
+ '(custom-safe-themes
+   (quote
+    ("99c86852decaeb0c6f51ce8bd46e4906a4f28ab4c5b201bdc3fdf85b24f88518" "80930c775cef2a97f2305bae6737a1c736079fdcc62a6fdf7b55de669fbbcd13" "446cc97923e30dec43f10573ac085e384975d8a0c55159464ea6ef001f4a16ba" "44961a9303c92926740fc4121829c32abca38ba3a91897a4eab2aa3b7634bed4" default)))
+ '(evil-collection-setup-minibuffer t)
+ '(org-agenda-files
+   (quote
+    ("c:/Users/Mantissa/Docs/Org/semester/semester.org" "c:/Users/Mantissa/Docs/Org/semester/Rodsem/notebook.org" "c:/Users/Mantissa/Docs/Org/semester/MSE4055/notebook.org" "c:/Users/Mantissa/Docs/Org/semester/iGEM/notebook.org" "c:/Users/Mantissa/Docs/Org/semester/CS2150/notebook.org" "c:/Users/Mantissa/Docs/Org/semester/CS2102/notebook.org" "c:/Users/Mantissa/Docs/Org/semester/BIOL3030/notebook.org" "c:/Users/Mantissa/Docs/Org/notes.org" "c:/Users/Mantissa/Docs/Org/Journal/20180828.org" "c:/Users/Mantissa/Docs/Org/Journal/20180827.org" "c:/Users/Mantissa/Docs/Org/Journal/20180826.org" "c:/Users/Mantissa/Docs/Org/Journal/20180825.org" "c:/Users/Mantissa/Docs/Org/Journal/20180824.org" "c:/Users/Mantissa/Docs/Org/Journal/20180823.org" "c:/Users/Mantissa/Docs/Org/Journal/20180822.org" "c:/Users/Mantissa/Docs/Org/Journal/20180821.org" "c:/Users/Mantissa/Docs/Org/Journal/20180820.org" "c:/Users/Mantissa/Docs/Org/Journal/20180818.org" "c:/Users/Mantissa/Docs/Org/Journal/20180816.org" "c:/Users/Mantissa/Docs/Org/Journal/20180814.org" "c:/Users/Mantissa/Docs/Org/Journal/20180813.org" "c:/Users/Mantissa/Docs/Org/Journal/20180812.org" "c:/Users/Mantissa/Docs/Org/Journal/20180811.org" "c:/Users/Mantissa/Docs/Org/Journal/20180810.org" "c:/Users/Mantissa/Docs/Org/idea_journal.org" "c:/Users/Mantissa/Docs/Org/Getting Started with Orgzly.org")))
+ '(package-selected-packages
+   (quote
+    (neotree postip company-quickhelp evil-org evil-org-mode mixed-pitch org-evil company-lsp company-lean lean-mode company ripgrep web-mode evil-mc evi-mc multishell multi-term evil-magit counsel-dash projectile lsp-javascript-typescript emacs-lsp evil-collection org-journal counsel-rg counsel swiper evil use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
