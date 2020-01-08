cc7d64779cc4143aa65d2ae38e3e010ae895aa62
Dylan Culfogienis <dylanculfogienis@gmail.com>
2020-01-07 21:21:31 -0500

    Added polybar config for bspwm

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Terminate running bar instances
+killall -q polybar
+
+# Wait until proceses have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+polybar top &
