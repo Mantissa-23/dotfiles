04c70f1302961a7cf0bc8b3883121cc88e94ff8e
Mantissa-23 <dylanculfogienis@gmail.com>
2022-01-16 15:56:57 -0700

    Fixed some project.nvim and tree.nvim settings that were bugging me

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -156,6 +156,8 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.update_cwd = false
+lvim.builtin.project.manual_mode = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
